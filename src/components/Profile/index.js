cb16a8fbd82b6bc55f39772578bf3f0f925ae865
Bruno Romeiro <bruno.romeiro@totvs.com.br>
2021-02-03 14:00:03 -0300

    feat: 🎸 create Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Matt Foo</h1>
+        <h2>Frontend Engenieer</h2>
+        <p>Membro do Core Team da empresa FOO, escrevo sobre frontend e outras coisas mais.</p>
+    </div>
+)
+
+export default Profile
