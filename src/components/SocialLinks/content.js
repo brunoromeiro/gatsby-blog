a8e74373971e3761dd5d676ea8b95dc46a461d7c
Bruno Romeiro <bruno.romeiro@totvs.com.br>
2021-02-03 18:09:28 -0300

    feat: 🎸 create SocialLinks compoenent

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = 'brunoromeiro'
+
+const links = [
+  {
+    label: 'Github',
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${handle}`
+  },
+  {
+    label: 'Instagram',
+    url: `https://www.instagram.com/${handle}`
+  },
+  {
+    label: 'Linkedin',
+    url: `https://www.linkedin.com/in/${handle}`
+  },
+  {
+    label: 'Twitch',
+    url: `https://www.twitch.tv/${handle}_`
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,15 @@
+import { Github } from '@styled-icons/boxicons-logos/Github'
+import { Twitter } from '@styled-icons/boxicons-logos/Twitter'
+import { Instagram } from '@styled-icons/boxicons-logos/Instagram'
+import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin'
+import { Twitch } from '@styled-icons/boxicons-logos/Twitch'
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram,
+  Linkedin,
+  Twitch
+}
+
+export default Icons
