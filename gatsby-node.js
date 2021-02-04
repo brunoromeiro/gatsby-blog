0a17f42d7fc48c7edf946349b25fc80682143dd7
Bruno Romeiro <bruno.romeiro@gmail.com>
2021-02-04 18:32:36 -0300

    refactor: 💡 create pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,19 +27,29 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
+  {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          fields {
+            slug
           }
+          frontmatter {
+            background
+            category
+            title
+            description
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+          }
+          timeToRead
         }
       }
     }
+  }
   `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -49,6 +59,24 @@ exports.createPages = ({ graphql, actions }) => {
           slug: node.fields.slug,
         },
       })
+
+      const postsPerPage = 6
+      const numPages = Math.ceil(posts.length / postsPerPage)
+
+      Array.from({ length: numPages }).forEach((_, index) => {
+        createPage({
+          path: index === 0 ? '/' : `/page/${index + 1}`,
+          component: path.resolve('./src/templates/blog-list.js'),
+          context: {
+            limit: postsPerPage,
+            skip: index * postsPerPage,
+            currentPage: index + 1
+          }
+        })
+      })
     })
   })
 }
+
+// To Create Pagination
+
