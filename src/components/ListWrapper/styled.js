eb4bad2547ac59cf272fb2223c7f5dc9a9b8c8a7
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 19:24:00 -0300

    feat: modernize post grid

--- src/components/ListWrapper/styled.js
+++ src/components/ListWrapper/styled.js
@@ -1,12 +1,25 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const ListWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 0 auto;
+  max-width: 1100px;
+  width: 100%;
+
   body#grid & {
-    background-color: var(--borders);
-    border-bottom: 1px solid var(--borders);
     display: grid;
-    grid-area: posts;
-    grid-gap: 1px;
-    grid-template-columns: repeat(auto-fit, minmax(380px, 1fr));
+    gap: 1.25rem;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
+
+  ${media.lessThan("large")`
+    gap: 0.875rem;
+
+    body#grid & {
+      grid-template-columns: 1fr;
+    }
+  `}
 `
