8770c90e35a136383abff579cf56f49011bf13dd
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 19:23:55 -0300

    feat: modernize sidebar layout

--- src/components/Sidebar/styled.js
+++ src/components/Sidebar/styled.js
@@ -1,22 +1,30 @@
-import styled from "styled-components"
-import media from "styled-media-query"
+import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const SidebarWrapper = styled.aside`
   align-items: center;
-  border-right: 1px solid var(--borders);
   background: var(--mediumBackground);
+  border-right: 1px solid var(--borders);
   display: flex;
   flex-direction: column;
   height: 100vh;
+  justify-content: flex-start;
+  left: 0;
+  padding: 2.5rem 1.5rem;
   position: fixed;
-  padding: 2rem;
   text-align: center;
+  top: 0;
   width: 20rem;
+  z-index: 20;
 
   ${media.lessThan("large")`
-    align-items: flex-start;
+    align-items: center;
+    border-bottom: 1px solid var(--borders);
+    border-right: 0;
+    flex-direction: row;
     height: auto;
-    padding: 1rem 2rem;
+    justify-content: space-between;
+    padding: 0.75rem 1rem;
     width: 100%;
   `}
 `
