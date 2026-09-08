7da5db742ab407f2d27becd3b58f239f29bc4b32
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 20:36:33 -0300

    feat: ajust package errors

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import ReactDisqusComments from 'react-disqus-comments'
 
 import * as S from './styled'
 
 const Comments = ({ url, title }) => {
   const completeUrl = `https://brunoromeiro.com${url}`
 
+  useEffect(() => {
+    if (typeof window === 'undefined') return undefined
+
+    const configureDisqus = function () {
+      this.page.url = completeUrl
+      this.page.identifier = completeUrl
+      this.page.title = title
+    }
+
+    if (window.DISQUS) {
+      window.DISQUS.reset({
+        reload: true,
+        config: configureDisqus,
+      })
+      return undefined
+    }
+
+    window.disqus_config = configureDisqus
+
+    const script = document.createElement('script')
+    script.id = 'disqus-embed-script'
+    script.src = 'https://disqus_z3dsKV8Xnk.disqus.com/embed.js'
+    script.setAttribute('data-timestamp', String(Date.now()))
+    script.async = true
+    document.body.appendChild(script)
+
+    return () => {
+      script.remove()
+    }
+  }, [completeUrl, title])
+
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
-      <ReactDisqusComments
-        shortname="disqus_z3dsKV8Xnk"
-        identifier={completeUrl}
-        title={title}
-        url={completeUrl}
-      />
+      <div id="disqus_thread" />
     </S.CommentsWrapper>
   )
 }
