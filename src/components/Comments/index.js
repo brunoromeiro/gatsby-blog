a96e93fe3f4bf5ae89d206071870605584d871ee
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 17:46:59 -0300

    fix: correct Comments prop type declaration

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import propTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import ReactDisqusComments from 'react-disqus-comments'
 
 import * as S from './styled'
 
-const Comments = ({url, title}) => {
+const Comments = ({ url, title }) => {
   const completeUrl = `https://brunoromeiro.com${url}`
+
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
@@ -18,9 +19,10 @@ const Comments = ({url, title}) => {
     </S.CommentsWrapper>
   )
 }
-Comments.prototype = {
-  url: propTypes.string.isRequired,
-  title: propTypes.string.isRequired
+
+Comments.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
 export default Comments
