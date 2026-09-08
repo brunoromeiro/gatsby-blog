30d6e47ed5d1c3bf18972f1f633a6a414dd338b6
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 17:41:55 -0300

    refactor: remove legacy transition portal

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,20 @@
-import React from "react"
-import { TransitionPortal } from "gatsby-plugin-transition-link"
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import PropTypes from "prop-types"
 import Sidebar from '../Sidebar'
 import MenuBar from '../MenuBar'
 
 import GlobalStyles from '../../styles/global'
 import * as S from './styled'
 
-const Layout = ({ children }) => {
-  
-  return (
-    <S.LayoutWrapper>
-      <GlobalStyles />
-      <TransitionPortal level="top">
-        <Sidebar />
-      </TransitionPortal>
-      <S.LayoutMain>{children}</S.LayoutMain>
-      <TransitionPortal level="top">
-        <MenuBar />
-      </TransitionPortal>
-    </S.LayoutWrapper>
-  )
-}
+const Layout = ({ children }) => (
+  <S.LayoutWrapper>
+    <GlobalStyles />
+    <Sidebar />
+    <S.LayoutMain>{children}</S.LayoutMain>
+    <MenuBar />
+  </S.LayoutWrapper>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
