ed1ca90e69ab2991ca1c1fd96a926c81399ae2ac
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 21:09:43 -0300

    feat: add initial posts collection

---
+++ src/collections/Posts.ts
@@ -0,0 +1,31 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  admin: {
+    useAsTitle: 'title',
+  },
+  versions: {
+    drafts: true,
+    maxPerDoc: 50,
+  },
+  fields: [
+    { name: 'title', type: 'text', required: true },
+    { name: 'slug', type: 'text', required: true, unique: true, index: true },
+    { name: 'excerpt', type: 'textarea' },
+    { name: 'content', type: 'richText', required: true },
+    { name: 'publishedAt', type: 'date', index: true },
+    {
+      name: 'status',
+      type: 'select',
+      defaultValue: 'draft',
+      required: true,
+      options: [
+        { label: 'Draft', value: 'draft' },
+        { label: 'Review', value: 'review' },
+        { label: 'Scheduled', value: 'scheduled' },
+        { label: 'Published', value: 'published' },
+      ],
+    },
+  ],
+}
