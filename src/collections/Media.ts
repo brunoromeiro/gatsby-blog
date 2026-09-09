da13b8c80268c01b735573973e84d858bca67d65
Bruno Romeiro <bruno.romeiro@gmail.com>
2026-09-08 21:09:47 -0300

    feat: add initial media collection

---
+++ src/collections/Media.ts
@@ -0,0 +1,13 @@
+import type { CollectionConfig } from 'payload'
+
+export const Media: CollectionConfig = {
+  slug: 'media',
+  upload: {
+    staticDir: 'media',
+    mimeTypes: ['image/*', 'video/*', 'image/gif'],
+  },
+  fields: [
+    { name: 'alt', type: 'text', required: true },
+    { name: 'caption', type: 'text' },
+  ],
+}
